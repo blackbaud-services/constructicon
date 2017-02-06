8abc3dbafbe74bd873b3c4566096fe25e93d47a6
Rowan Hogan <rowan.hogan@everydayhero.com>
2017-02-07 08:57:42 +1000

    Add carousel tests

--- test/testdom.js
+++ test/testdom.js
@@ -3,6 +3,16 @@ module.exports = function(markup) {
   var jsdom = require('jsdom').jsdom
   global.document = jsdom(markup || '')
   global.window = document.defaultView
+
+  // Stub `matchMedia` to silence errors
+  global.window.matchMedia = global.window.matchMedia || function() {
+    return {
+      matches : false,
+      addListener : function() {},
+      removeListener: function() {}
+    }
+  }
+
   global.navigator = {
     userAgent: 'chrome'
   }
