8dd0e8dd49f1f4acea3677cfbe4b5929855a219b
danhenderson <danhenderson.web@gmail.com>
2018-02-14 16:20:50 +1000

    Add validations component

---
+++ source/components/input-validations/index.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import withStyles from '../with-styles'
+import styles from './styles'
+
+const InputValidations = ({
+  classNames,
+  validations = []
+}) => (
+  <div className={`c11n-input-validations ${classNames.root}`}>
+    {validations.map((error, i) => (
+      <div className={classNames.error} key={i}>
+        {error}
+      </div>
+    ))}
+  </div>
+)
+
+InputValidations.propTypes = {
+  /**
+  * An array of input validations
+  */
+  validations: PropTypes.array,
+
+  /**
+  *
+  */
+  styles: PropTypes.object
+
+}
+
+InputValidations.defaultProps = {}
+
+export default withStyles(styles)(InputValidations)
