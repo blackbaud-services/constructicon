bf0a43b459013ab1ca81833f4876421ee582cd69
danhenderson <danhenderson.web@gmail.com>
2018-02-14 14:39:33 +1000

    Add Label component

---
+++ source/components/label/Readme.md
@@ -0,0 +1,30 @@
+# Examples
+
+**Standard Use**
+
+```
+<Label>My Field</Label>
+```
+
+**Required**
+
+```
+<Label required>My Field</Label>
+```
+
+**Custom styles**
+
+Apply a custom styles object to alter the look. Available elements are:
+
+- `root` - Label element
+- `required` - Required label (if present)
+
+```
+var styles = {
+  root: {
+    color: 'green'
+  }
+};
+
+<Label styles={styles}>Green Label</Label>
+```
---
+++ source/components/label/index.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import withStyles from '../with-styles'
+import styles from './styles'
+
+const Label = ({
+  children,
+  classNames,
+  id,
+  inputId,
+  required
+}) => (
+  <label className={`c11n-label ${classNames.root}`} id={id} htmlFor={inputId}>
+    {children}
+    {required && <span className={classNames.required} title='Required field'>*</span>}
+  </label>
+)
+
+Label.propTypes = {
+  /**
+  * The label
+  */
+  children: PropTypes.any,
+
+  /**
+  * The id of the label
+  */
+  id: PropTypes.string,
+
+  /**
+  * The id of the related input
+  */
+  inputId: PropTypes.string,
+
+  /**
+  * Whether the field is required
+  */
+  required: PropTypes.bool
+}
+
+Label.defaultProps = {}
+
+export default withStyles(styles)(Label)
