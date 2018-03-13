d08c3d30c5587e80d3509f5af5ddb8037f7b1320
Rowan Hogan <rowan.hogan@everydayhero.com>
2018-03-13 11:24:05 +1000

    Add a check for ".only" in tests as a pipline step. Closes #119

--- source/components/with-styles/index.js
+++ source/components/with-styles/index.js
@@ -1,24 +1,12 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
-import cxs from 'cxsync'
+import { stylesToClasses, addKeyframes } from '../../lib/css'
 import * as defaultTraits from '../../lib/traits'
-
-export const css = cxs
-
-/**
-* Turns a styles object into an object containing the cxs class name for each key
-*/
-export const stylesToClasses = (styles = {}) => (
-  Object.keys(styles).reduce((acc, key) => ({
-    ...acc,
-    [key]: css(styles[key])
-  }), {})
-)
 
 /**
 * Higher order component to take a styles function and call it with the necessary props and traits
 */
-const withStyles = (styles) => (ComponentToWrap) => {
+const withStyles = (styles, keyframes = {}) => (ComponentToWrap) => {
   class ConnectStyles extends Component {
     render () {
       // get current traits and defaults from context
@@ -37,11 +25,16 @@ const withStyles = (styles) => (ComponentToWrap) => {
       const stylesIsFunction = typeof styles === 'function'
       const stylesObj = stylesIsFunction ? styles(combinedProps, traits) : styles
 
+      // add any keyframes
+      const keyframesIsFunction = typeof keyframes === 'function'
+      const keyframesObj = keyframesIsFunction ? keyframes(combinedProps, traits) : keyframes
+      addKeyframes(keyframesObj)
+
       // build out our final props to be passed down to the original component
       const newProps = {
         ...combinedProps,
         styles: stylesObj,
-        classNames: stylesToClasses(stylesObj)
+        classNames: stylesToClasses(stylesObj),
       }
 
       return (
