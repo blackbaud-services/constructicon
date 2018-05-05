c0c406dee30c66472e5afe02eeb60298b76a9957
danhenderson <danhenderson.web@gmail.com>
2018-05-06 08:29:45 +1000

    Strip out nested styles

---
+++ source/lib/css/fela-strip-nested/index.js
@@ -0,0 +1,12 @@
+export default () => (styles) => (
+  Object.keys(styles).reduce((result, key) => {
+    const isNested = typeof styles[key] === 'object'
+    const isCSSSelector = key.indexOf('&') === 0
+    const isMediaQuery = key.indexOf('@') === 0
+    const isKeyframe = key === 'from' || key === 'to' || key.indexOf('%') !== -1
+
+    return isNested && !isCSSSelector && !isMediaQuery && !isKeyframe
+      ? result
+      : { ...result, [key]: styles[key] }
+  }, {})
+)
