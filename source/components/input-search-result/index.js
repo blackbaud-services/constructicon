e12a872e07252ca79d3be05f3fb41870d248ea5e
danhenderson <danhenderson.web@gmail.com>
2018-10-15 16:13:40 +1000

    Add input search result component

---
+++ source/components/input-search-result/index.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import withStyles from '../with-styles'
+import styles from './styles'
+import Section from '../section'
+
+const InputSearchResult = ({ children, isActive, spacing, styles }) => (
+  <Section
+    background={isActive ? 'shade' : 'light'}
+    spacing={spacing}
+    styles={styles.root}
+  >
+    {children}
+  </Section>
+)
+
+InputSearchResult.propTypes = {
+  /**
+   * The contents of the search result
+   */
+  children: PropTypes.any.isRequired,
+
+  /**
+   * Whether the item is the actively selected result
+   */
+  isActive: PropTypes.bool,
+
+  /**
+   * The spacing to be applied to the result
+   */
+  spacing: PropTypes.oneOf([PropTypes.object, PropTypes.number]),
+
+  /**
+   * Custom styles to be applied
+   */
+  styles: PropTypes.object
+}
+
+InputSearchResult.defaultProps = {
+  spacing: { x: 1, y: 0.5 }
+}
+
+export default withStyles(styles)(InputSearchResult)
