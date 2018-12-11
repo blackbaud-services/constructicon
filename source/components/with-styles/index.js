78d9d247f2cc81313e8f9f2acc8a2c2cabe8a223
Rowan Hogan <rowan.hogan@everydayhero.com>
2018-12-11 12:59:41 +1000

    Add ability to opt-out of built-in styles by passing a classNames prop

--- source/components/with-styles/index.js
+++ source/components/with-styles/index.js
@@ -9,35 +9,45 @@ import * as defaultTraits from '../../lib/traits'
 const withStyles = (styles, keyframes = {}) => ComponentToWrap => {
   class ConnectStyles extends Component {
     render () {
-      // get current traits and defaults from context
+      // Get current traits and defaults from context
       const { traits = defaultTraits } = this.context
 
-      // build our combined props from the component itself's default props,
-      // the specified default traits, and the actual provided props
+      // Build our combined props from the component itself's default props,
+      // The specified default traits, and the actual provided props
       const combinedProps = {
         ...ComponentToWrap.defaultProps,
         ...this.props
       }
 
-      // add any keyframes
+      // Add any keyframes
       const keyframesIsFunction = typeof keyframes === 'function'
       const keyframesObject = keyframesIsFunction
         ? keyframes(combinedProps, traits)
         : keyframes
       const keyframeNames = addKeyframes(keyframesObject)
 
-      // if styles is a function, call it and pass through our props and traits
+      // Opt out of built-in styles
+      if (typeof this.props.classNames === 'object') {
+        const newProps = {
+          ...combinedProps,
+          styles: {},
+          classNames: this.props.classNames
+        }
+
+        return <ComponentToWrap {...newProps} />
+      }
+
+      // If styles is a function, call it and pass through our props and traits
       const stylesIsFunction = typeof styles === 'function'
       const stylesObject = stylesIsFunction
         ? styles(combinedProps, traits, keyframeNames)
         : styles
-      const classNames = stylesToClasses(stylesObject)
 
-      // build out our final props to be passed down to the original component
+      // Build out our final props to be passed down to the original component
       const newProps = {
         ...combinedProps,
         styles: stylesObject,
-        classNames
+        classNames: stylesToClasses(stylesObject)
       }
 
       return <ComponentToWrap {...newProps} />
