2854d7eaf037facf7d4699d109b2f8a0556f4bfb
Rowan Hogan <rowan.hogan@gmail.com>
2020-07-13 13:56:07 +1000

    Use dependency for ticker

--- source/components/ticker/__tests__/Ticker-test.js
+++ source/components/ticker/__tests__/Ticker-test.js
@@ -5,23 +5,14 @@ import { colors } from '../../../lib/traits'
 describe('Ticker', () => {
   it('renders a simple ticker', () => {
     const wrapper = mount(<Ticker label='Label' items={['Example item']} />)
-    const ticker = wrapper.find('Ticker')
+    const ticker = wrapper.find('Ticker').first()
     const labelClass = ticker.prop('classNames').label
-    const itemsClass = ticker.prop('classNames').items
     expect(wrapper.find(`.${labelClass}`).text()).to.eql('Label')
-    expect(wrapper.find(`.${itemsClass}`).text()).to.contain('Example item')
-  })
-
-  it('renders items containing elements', () => {
-    const wrapper = mount(<Ticker items={[<div>Hello</div>]} />)
-    const ticker = wrapper.find('Ticker')
-    const itemsClass = ticker.prop('classNames').items
-    expect(wrapper.find(`.${itemsClass}`).text()).to.contain('Hello')
   })
 
   it('allows us to set the background color', () => {
     const wrapper = mount(<Ticker background='secondary' items={['Test']} />)
-    const ticker = wrapper.find('Ticker')
+    const ticker = wrapper.find('Ticker').first()
     const styles = ticker.prop('styles')
     expect(styles.root.backgroundColor).to.eql(colors.secondary)
   })
