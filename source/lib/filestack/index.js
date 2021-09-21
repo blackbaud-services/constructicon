5eda4bed3ca947238551a83efa4f3e1dfe21349c
Rowan Hogan <rowan.hogan@gmail.com>
2021-09-21 15:08:09 +1000

    Remove filestack dependency

--- source/lib/filestack/index.js
+++
@@ -1,9 +0,0 @@
-import * as filestack from 'filestack-js'
-import { dataURIToBlob } from '../files'
-
-export const uploadToFilestack = (image, key) => {
-  const client = filestack.init(process.env.FILESTACK_API_KEY)
-  const file = dataURIToBlob(image)
-  const filename = `upload-${new Date().toISOString()}-${key}.jpg`
-  return client.upload(file, {}, { filename }).then(res => res.url)
-}
