8239ba8cae81d61b1c774fb6b08cc09f3bb44746
Lewis <lewis@lewiscodes.com>
2022-12-16 12:53:45 +0000

    2377614: increased zIndex of modal so it is higher than the cookie banner (should be the highest element on the page)

--- source/components/modal/styles.js
+++ source/components/modal/styles.js
@@ -8,7 +8,7 @@ export default ({ spacing, styles, width }, { calculateSpacing, rhythm }) => {
       left: 0,
       right: 0,
       bottom: 0,
-      zIndex: 100,
+      zIndex: 999,
       backgroundColor: 'rgba(0, 0, 0, 0.75)'
     },
 
