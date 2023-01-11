25576383bcafcb63507c88432b22e243481f0dd8
Lewis <lewis@lewiscodes.com>
2023-01-11 10:14:38 +0000

    added onClick prop to share-button

--- source/components/button-share/index.js
+++ source/components/button-share/index.js
@@ -23,7 +23,7 @@ const ButtonShare = ({
   <Button
     background={type}
     tag={share ? 'button' : 'a'}
-    onClick={share && openShareDialog({ type, url, title, hashtags, image, onClick })}
+    onClick={share && openShareDialog({ type, url, title, hashtags, image })}
     aria-label={`${share ? 'Share' : 'View'} on ${type}`}
     {...props}
   >
