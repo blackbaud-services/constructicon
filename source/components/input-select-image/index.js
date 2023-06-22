c301f95547d871a3bd33ad5029d800a57a32a706
Lewis <lewis@lewiscodes.com>
2023-06-22 10:54:07 +0100

    2542910: added imageSelect component

---
+++ source/components/input-select-image/index.js
@@ -0,0 +1,60 @@
+import React, { useState } from 'react'
+import Label from '../label'
+import withStyles from '../with-styles'
+import InputValidations from '../input-validations'
+import styles from './styles'
+
+const ImageSelect = ({ label, id, name, required, options, classNames, disabled, onChange, onBlur, error, validations, ...props}) => {
+  const inputId = id || name
+  const labelId = `label-${inputId}`
+  const [selectedItem, setSelectedItem] = useState()
+  const handleChange = value => {
+    setSelectedItem(value)
+    onChange && onChange(value)
+  }
+
+  return <div className={classNames.root}>
+    {label && (
+      <Label
+        id={labelId}
+        inputId={inputId}
+        required={required}
+      >
+        {label}
+      </Label>
+    )}
+    <div id={inputId}>
+      {options.map(option =>
+        <ImageSelectItem
+          key={option.value}
+          classNames={classNames}
+          onClick={() => handleChange(option.value)}
+          onBlur={() => onBlur && onBlur(option.value)}
+          selected={selectedItem === option.value}
+          disabled={disabled}
+          {...option}
+        />
+      )}
+    </div>
+    {error && (
+      <InputValidations
+        styles={{ root: styles.error }}
+        validations={validations}
+      />
+    )}
+  </div>
+}
+
+const ImageSelectItem = ({ classNames, image, value, selected, onClick, onBlur, disabled, ...props }) => {
+  const classes = `${classNames.option} ${selected && classNames.optionSelected} ${disabled && classNames.disabled}`
+
+  return (
+    <div className={classes} onClick={() => !disabled && onClick()}>
+      <input type='radio' checked={selected} onBlur={onBlur} className={classNames.radio} disabled={disabled} readonly={disabled} />
+      <img src={image} alt={value} />
+      <p className={classNames.optionLabel}>{value}</p>
+    </div>
+  )
+}
+
+export default withStyles(styles)(ImageSelect)
